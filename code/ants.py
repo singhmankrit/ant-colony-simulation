df71e731b61bf6c2fa7cec8f2337ce1bcdd5e8d9
Mankrit Singh <m.singh-6@student.tudelft.nl>
2025-05-23 00:40:00 +0200

    pheromone dilution

--- code/ants.py
+++ code/ants.py
@@ -7,7 +7,7 @@ class Ant:
         self.grid = grid
         self.pos = grid.colony_position  # assume all ants start at colony
         self.path_memory = [self.pos]
-        self.return_path_length = 0
+        self.pheromone_strength = 0
 
         # ant status
         self.has_food = False
@@ -20,13 +20,15 @@ class Ant:
 
         if not self.has_food and self.pos in self.grid.food_positions:
             self.has_food = True
-            self.return_path_length = len(self.path_memory)
+            path_length = len(self.path_memory)
+            if path_length > 0:
+                self.pheromone_strength = 1 / path_length
             print(f"Ant picked up food at step: {step_number}")
 
         elif self.has_food and self.pos == self.grid.colony_position:
             self.has_food = False
             self.path_memory = []
-            self.return_path_length = 0
+            self.pheromone_strength = 0
             print(f"Ant dropped food at step: {step_number}")
 
         # print("Ant moved to", self.pos)
@@ -69,12 +71,6 @@ class Ant:
         self.pos = chosen_pos
 
     def travel_back(self):
-        distance_from_food = len(self.path_memory)
-        if self.return_path_length > 0:
-            strength = distance_from_food / self.return_path_length
-        else:
-            strength = 0  # unlikely case: means food is at colony
-
         chosen_pos = self.path_memory.pop()
-        self.grid.add_pheromone(chosen_pos, strength=strength)
+        self.grid.add_pheromone(chosen_pos, strength=self.pheromone_strength)
         self.pos = chosen_pos
--- journal.md
+++ journal.md
@@ -70,7 +70,7 @@ References:
 
 - I worked on the basic simulation for the project this week, done in !1. This did not have any pheromone based behaviour and was a simple random walk for 1 ant, but it sets a flexible code design for our project.
 - Then I added pheromone related behaviour in !2, and improved plots. This also has the feature to evaporate pheromones.
-- Finally, I added pheromone decay across length of the ant path and improved some outcome logging in !3.
+- Finally, I added pheromone dilution across length of the ant path and improved some outcome logging in !3.
 
 Currently, the simulation looks like this:
 ![](./journal/week2/ant_simulation.png)
