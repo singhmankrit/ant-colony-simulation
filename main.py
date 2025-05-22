b9d44e599f0c7868216434e7a31a363004b13b8c
Mankrit Singh <m.singh-6@student.tudelft.nl>
2025-05-22 12:01:35 +0200

    update food scent

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import code.grid as grid
 import code.ants as ants
 
-environment = grid.Grid(5)
+environment = grid.Grid(9)
 environment.place_colony((0, 0))
 environment.add_food((4, 4))
+environment.set_food_scent()
 
 environment.add_obstacle((3, 2))
 environment.add_obstacle((3, 3))
 environment.add_obstacle((2, 3))
 print(environment.grid)
+print("Food Scent: \n")
+print(environment.food_scent)
 
 ant = ants.Ant(environment)
 for i in range(10):
