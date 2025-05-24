575af4b613f4087013944481db40747bdd0be6c8
Noa Aarts <noa@voorwaarts.nl>
2025-05-24 22:10:13 +0200

    we didn't use self.grid anywhere

--- code/grid.py
+++ code/grid.py
@@ -1,18 +1,9 @@
 import numpy as np
-from enum import Enum
-
-
-class CellType(Enum):
-    EMPTY = 0
-    COLONY = 1
-    FOOD = 2
-    OBSTACLE = 3
 
 
 class Grid:
     def __init__(self, size):
         self.size = size
-        self.grid = np.zeros((size, size), dtype=int)
         self.food_scent = np.zeros((size, size), dtype=float)  # natural scent
         self.food_path = np.zeros((size, size), dtype=float)  # pheromones
 
@@ -22,15 +13,12 @@ class Grid:
 
     def place_colony(self, position):
         self.colony_position = position
-        self.grid[position] = CellType.COLONY.value
 
     def add_food(self, position):
         self.food_positions.append(position)
-        self.grid[position] = CellType.FOOD.value
 
     def add_obstacle(self, position):
         self.obstacle_positions.append(position)
-        self.grid[position] = CellType.OBSTACLE.value
 
     def set_food_scent(self):
         """
